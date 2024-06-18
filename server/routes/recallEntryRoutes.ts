86440b10707dfcd4992ffb535c5f48e97b93c906
yasin <yasin.mustafa@gmail.com>
2024-06-18 13:14:38 +0100

    Correct lint error

--- server/routes/recallEntryRoutes.ts
+++ server/routes/recallEntryRoutes.ts
@@ -4,8 +4,6 @@ export default class RecallEntryRoutes {
   constructor() {}
 
   public getEnterRecallDate: RequestHandler = async (req, res): Promise<void> => {
-    const { nomsId } = req.params
-    const { token } = res.locals.user
     return res.render('pages/recallEntry/enter-recall-date')
   }
 }
