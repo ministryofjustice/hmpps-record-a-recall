787b3a822fa20efd6907791b84e0c58132340719
Yasin Mustafa <yasin.mustafa@gmail.com>
2024-06-21 09:24:54 +0100

    RCLL-31 Create POC version of the return-to-custody-date screen (#11)
    * RCLL-31 Create POC version of the  return-to-custody-date screen
    
    * Pass nomsId to njk file

--- server/routes/recallEntryRoutes.ts
+++ server/routes/recallEntryRoutes.ts
@@ -4,6 +4,12 @@ export default class RecallEntryRoutes {
   constructor() {}
 
   public getEnterRecallDate: RequestHandler = async (req, res): Promise<void> => {
-    return res.render('pages/recallEntry/enter-recall-date')
+    const { nomsId } = req.params
+    return res.render('pages/recallEntry/enter-recall-date', { nomsId })
+  }
+
+  public getEnterReturnToCustodyDate: RequestHandler = async (req, res): Promise<void> => {
+    const { nomsId } = req.params
+    return res.render('pages/recallEntry/enter-return-to-custody-date', { nomsId })
   }
 }
