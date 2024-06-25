98b3099ce67f4a3016917148e27b1032b8b01f35
yasin <yasin.mustafa@gmail.com>
2024-06-25 10:30:18 +0100

    remove unused util function

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -21,13 +21,3 @@ export const initialiseName = (fullName?: string): string | null => {
   const array = fullName.split(' ')
   return `${array[0][0]}. ${array.reverse()[0]}`
 }
-
-export const groupBy = <T, K>(items: T[], groupingFunction: (item: T) => K): Map<K, T[]> => {
-  return items.reduce((result, item) => {
-    const key = groupingFunction(item)
-    const currentValues = result.get(key) || []
-    currentValues.push(item)
-    result.set(key, currentValues)
-    return result
-  }, new Map<K, T[]>())
-}
