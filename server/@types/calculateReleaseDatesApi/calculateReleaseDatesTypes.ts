21a518e627dfee01e6dd4d7af20a09d32b695e6a
yasin <yasin.mustafa@gmail.com>
2024-08-20 16:32:30 +0100

    error handling from crd call

--- server/@types/calculateReleaseDatesApi/calculateReleaseDatesTypes.ts
+++ server/@types/calculateReleaseDatesApi/calculateReleaseDatesTypes.ts
@@ -3,4 +3,5 @@ import { components } from './index'
 export type AnalysedSentenceAndOffence = components['schemas']['AnalysedSentenceAndOffence']
 export type LatestCalculation = components['schemas']['LatestCalculation']
 export type CalculationBreakdown = components['schemas']['CalculationBreakdown']
-export type SentenceAndOffenceWithReleaseArrangements = components['schemas']['SentenceAndOffenceWithReleaseArrangements']
+export type SentenceAndOffenceWithReleaseArrangements =
+  components['schemas']['SentenceAndOffenceWithReleaseArrangements']
