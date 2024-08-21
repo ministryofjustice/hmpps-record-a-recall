bb426c16ecf1c9bbb725c0da8850cdf0e11ead09
yasin <yasin.mustafa@gmail.com>
2024-08-21 16:34:28 +0100

    add dps url to nunjusk setup

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -23,6 +23,7 @@ export default function nunjucksSetup(app: express.Express, applicationInfo: App
   app.locals.applicationName = 'Record A Recall'
   app.locals.environmentName = config.environmentName
   app.locals.environmentNameColour = config.environmentName === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
+  app.locals.digitalPrisonServicesUrl = config.digitalPrisonServicesUrl
 
   // Cachebusting version string
   if (production) {
