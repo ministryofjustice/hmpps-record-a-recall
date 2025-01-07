a812de948b24fcdf0a6a6208fbd1735a2bf3514e
danbenton-mojdt <113102670+danbenton-mojdt@users.noreply.github.com>
2025-01-07 15:49:01 +0000

    RCLL-152 Get breakdown and sentences before deciding if recall is possible (#109)
    * RCLL-152 Get breakdown and sentences before deciding if recall is possible
    
    * RCLL-152 Remove console logs

--- server/controllers/recall/recallBaseController.ts
+++ server/controllers/recall/recallBaseController.ts
@@ -1,20 +1,19 @@
-import { NextFunction, Response } from 'express'
+import { Response } from 'express'
 import FormWizard from 'hmpo-form-wizard'
 import { CalculatedReleaseDates } from '../../@types/calculateReleaseDatesApi/calculateReleaseDatesTypes'
 import PrisonerDetailsController from '../base/prisonerDetailsController'
-import logger from '../../../logger'
 import config from '../../config'
 
 export default class RecallBaseController extends PrisonerDetailsController {
   middlewareSetup() {
     super.middlewareSetup()
-    this.use(this.getTemporaryCalculation)
   }
 
   locals(req: FormWizard.Request, res: Response): Record<string, unknown> {
     const locals = super.locals(req, res)
     const calculation = req.sessionModel.get<CalculatedReleaseDates>('temporaryCalculation')
     const recallDate = req.sessionModel.get('recallDate')
+    const validationErrors = req.sessionModel.get('validationErrors')
 
     const urls = {
       recalls: `${config.applications.recordARecall.url}/person/${locals.nomisId}`,
@@ -24,31 +23,6 @@ export default class RecallBaseController extends PrisonerDetailsController {
       ccards: `${config.applications.courtCasesReleaseDates.url}/prisoner/${locals.nomisId}/overview`,
     }
 
-    return { ...locals, calculation, recallDate, urls }
-  }
-
-  async getTemporaryCalculation(req: FormWizard.Request, res: Response, next: NextFunction) {
-    const { nomisId, username, validationResponse } = res.locals
-
-    const temporaryCalculation = req.sessionModel.get<CalculatedReleaseDates>('temporaryCalculation')
-    if (!temporaryCalculation && (!validationResponse || validationResponse.length === 0)) {
-      req.services.calculationService
-        .calculateTemporaryDates(nomisId, username)
-        .then(newCalc => {
-          res.locals.temporaryCalculation = newCalc
-          logger.debug(newCalc.dates)
-
-          req.sessionModel.set('temporaryCalculation', newCalc)
-          req.sessionModel.save()
-          next()
-        })
-        .catch(error => {
-          req.sessionModel.unset('temporaryCalculation')
-          req.sessionModel.set('crdsError', error.userMessage)
-          next(error)
-        })
-    } else {
-      next()
-    }
+    return { ...locals, calculation, recallDate, urls, validationErrors }
   }
 }
