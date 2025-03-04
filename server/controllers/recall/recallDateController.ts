c8fe0211fc31b789fe2fadbe24d1ac15832b92a8
danbenton-mojdt <113102670+danbenton-mojdt@users.noreply.github.com>
2025-03-04 14:53:32 +0000

    RCLL-318 Validate recall date is after earliest sentence date (#164)
    * RCLL-318 Validate recall date is after earliest sentence date
    
    * RCLL-318 remove unused import

--- server/controllers/recall/recallDateController.ts
+++ server/controllers/recall/recallDateController.ts
@@ -1,10 +1,11 @@
 import FormWizard from 'hmpo-form-wizard'
 import { NextFunction, Response } from 'express'
 
+import { isBefore, min } from 'date-fns'
 import RecallBaseController from './recallBaseController'
 import { PrisonerSearchApiPrisoner } from '../../@types/prisonerSearchApi/prisonerSearchTypes'
 import recallDateCrdsDataComparison from '../../utils/recallDateCrdsDataComparison'
-import { getRecallOptions } from '../../helpers/formWizardHelper'
+import { getCrdsSentences, getRecallOptions } from '../../helpers/formWizardHelper'
 
 export default class RecallDateController extends RecallBaseController {
   locals(req: FormWizard.Request, res: Response): Record<string, unknown> {
@@ -13,6 +14,23 @@ export default class RecallDateController extends RecallBaseController {
 
     const backLink = `/person/${prisoner.prisonerNumber}${locals.isEditRecall ? `/recall/${locals.recallId}/edit/edit-summary` : ''}`
     return { ...locals, backLink }
+  }
+
+  validateFields(req: FormWizard.Request, res: Response, callback: (errors: unknown) => void) {
+    super.validateFields(req, res, errors => {
+      const { values } = req.form
+      const sentences = getCrdsSentences(req)
+
+      const earliestSentenceDate = min(sentences.map(s => new Date(s.sentenceDate)))
+      /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
+      const validationErrors: any = {}
+
+      if (isBefore(values.recallDate as string, earliestSentenceDate)) {
+        validationErrors.recallDate = this.formError('recallDate', 'mustBeAfterEarliestSentenceDate')
+      }
+
+      callback({ ...errors, ...validationErrors })
+    })
   }
 
   successHandler(req: FormWizard.Request, res: Response, next: NextFunction) {
