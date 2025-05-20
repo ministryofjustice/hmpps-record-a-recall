e7ce147afb39d40f9f6cc8dbac6243097bf751c8
Dan Benton <daniel.benton@digital.justice.gov.uk>
2025-05-20 14:39:50 +0100

    Fix CRDS url

--- server/helpers/urlHelper.ts
+++ server/helpers/urlHelper.ts
@@ -3,7 +3,7 @@ import config from '../config'
 export default function getServiceUrls(nomisId: string) {
   return {
     recalls: `${config.applications.recordARecall.url}/person/${nomisId}`,
-    crds: `${config.applications.calculateReleaseDates.url}/person/${nomisId}`,
+    crds: `${config.applications.calculateReleaseDates.url}/?prisonId=${nomisId}`,
     adjustments: `${config.applications.adjustments.url}/${nomisId}`,
     profile: `${config.applications.digitalPrisonServices.url}/prisoner/${nomisId}`,
     ccards: `${config.applications.courtCasesReleaseDates.url}/prisoner/${nomisId}/overview`,
