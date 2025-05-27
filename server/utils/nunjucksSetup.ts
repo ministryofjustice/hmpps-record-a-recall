1613fcb489cf54e884ddcd8acd35e9d967a18fe7
Jade Alvares <jade.alvares@justice.gov.uk>
2025-05-27 11:12:01 +0100

    RCLL-243 displays new offence card with hardcoded data

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -9,6 +9,7 @@ import {
   personStatus,
   firstNameSpaceLastName,
   formatLengths,
+  // consecutiveToDetailsToDescription,
 } from 'hmpps-court-cases-release-dates-design/hmpps/utils/utils'
 import dayjs from 'dayjs'
 import { initialiseName } from './utils'
@@ -77,6 +78,7 @@ export default function nunjucksSetup(app: express.Express, applicationInfo: App
   njkEnv.addFilter('datetime', (date, format = 'YYYY-MM-DD HH:mm:ss') => dayjs(date).format(format))
   njkEnv.addFilter('sentenceDate', (date, format = 'dddd, DD MMMM YYYY') => dayjs(date).format(format))
   njkEnv.addFilter('formatLengths', formatLengths)
+  // njkEnv.addFilter('consecutiveToDetailsToDescription', consecutiveToDetailsToDescription)
 
   // Filter to pluralize a word based on a count. Adds 's' if count is not 1.
   function pluralize(count: number): string {
