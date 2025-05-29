fee0f48e4e3884266994c416d2786bfc7e039ec7
ayo.adedeji <ayo.adedeji@justice.gov.uk>
2025-05-29 15:18:41 +0100

    Adds manual recall intercept page
    Implements an intercept page to inform users about the manual case selection process. This ensures users understand they must manually select relevant cases for recall when the service cannot automatically determine eligible sentences.

--- server/controllers/recall/ManualRecallInterceptController.ts
+++ server/controllers/recall/ManualRecallInterceptController.ts
@@ -5,12 +5,12 @@ import RecallBaseController from './recallBaseController'
 export default class ManualRecallInterceptController extends RecallBaseController {
   async get(req: FormWizard.Request, res: Response, next: NextFunction): Promise<void> {
     const locals = this.locals(req, res)
-    res.render('pages/recall/manual-recall-intercept.njk', locals)
+    res.render('pages/recall/manualRecallIntercept', locals)
   }
 
   async post(req: FormWizard.Request, res: Response, next: NextFunction): Promise<void> {
     if ('continue' in req.body) {
-      req.sessionModel.set('manualRecallInterceptConfirmation', 'confirmed')
+      return res.redirect(303, `${req.baseUrl}/select-cases`)
     }
     if ('cancel' in req.body) {
       return res.redirect(303, `${req.baseUrl}/confirm-cancel`)
