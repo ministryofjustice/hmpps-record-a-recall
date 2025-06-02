59a41aeef2bc9e2a4469c974ef996aa3c52fdec9
ayo.adedeji <ayo.adedeji@justice.gov.uk>
2025-06-02 13:40:14 +0100

    updated integration test

--- integration_tests/e2e/create-happy-path.cy.ts
+++ integration_tests/e2e/create-happy-path.cy.ts
@@ -51,9 +51,6 @@ context('Create recall happy path', () => {
     // Step 5: Select court cases
     const selectCasesTitle = 'Select court cases'
     cy.url().should('include', '/person/A1234AB/record-recall/select-cases')
-    const selectCasesForm = FormPage.verifyOnPage<FormPage>(FormPage, selectCasesTitle)
-    selectCasesForm.selectFirstCourtCaseCheckbox()
-    new ReviewFormPage(selectCasesTitle).continueButton().click()
 
     // Step 6: Check sentences
     const sentenceCheckTitle = 'Check that the sentences and offences are correct'
