9539d4674414e49bb1325ad7720b2cffa98a98ad
ayo.adedeji <ayo.adedeji@justice.gov.uk>
2025-06-02 13:19:43 +0100

    Log potential date formatting error

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -6,6 +6,7 @@ import { SummaryListRow } from '../@types/govuk'
 import toSummaryListRow from '../helpers/componentHelper'
 import { formatLongDate } from '../formatters/formatDate'
 import { RecallJourneyData } from '../helpers/formWizardHelper'
+import logger from '../../logger'
 
 const properCase = (word: string): string =>
   word.length >= 1 ? word[0].toUpperCase() + word.toLowerCase().slice(1) : word
@@ -69,6 +70,7 @@ export function formatDateStringToDDMMYYYY(isoDateString?: string): string {
     const dateObj = parseISO(isoDateString)
     return format(dateObj, 'dd/MM/yyyy')
   } catch (e) {
+    logger.error('Error formatting date string:', isoDateString, e)
     return 'Invalid date'
   }
 }
