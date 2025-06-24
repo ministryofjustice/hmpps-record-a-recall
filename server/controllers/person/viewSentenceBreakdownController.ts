fecd705359e0857968a965df41f129e2211cd6eb
ayoMoj <ayo.adedeji@justice.gov.uk>
2025-06-24 08:25:17 +0100

    Renames controller files for consistency (#233)
    Renames the `viewPersonHome.ts` and `viewSentenceBreakdown.ts` files to `viewPersonHomeController.ts` and `viewSentenceBreakdownController.ts` respectively for naming consistency across the project.
    
    Updates imports accordingly.

--- server/controllers/person/viewSentenceBreakdownController.ts
+++ server/controllers/person/viewSentenceBreakdownController.ts
@@ -4,6 +4,7 @@ import {
   CalculatedReleaseDates,
   LatestCalculation,
 } from '../../@types/calculateReleaseDatesApi/calculateReleaseDatesTypes'
+import { setPrisonerDetailsInLocals } from './viewPersonHomeController'
 import CalculationService from '../../services/calculationService'
 
 export default async (req: Request, res: Response) => {
