1e712de885828471d4f21880a629366cb66a384b
Jade Alvares <jade.alvares@justice.gov.uk>
2025-06-25 16:58:16 +0100

    RCLL-250 put logic to offenceNameMap in loadCourtCases

--- server/controllers/person/viewPersonHomeController.ts
+++ server/controllers/person/viewPersonHomeController.ts
@@ -5,7 +5,7 @@ import getServiceUrls from '../../helpers/urlHelper'
  * ViewPersonHomeController - Displays the person home page with recall information
  */
 export default async (req: Request, res: Response) => {
-  const { nomisId, prisoner, recalls, serviceDefinitions, banner, errorMessage, recallableCourtCases } = res.locals
+  const { nomisId, prisoner, recalls, serviceDefinitions, banner, errorMessage, recallableCourtCases, } = res.locals
 
   if (prisoner) {
     const urls = getServiceUrls(nomisId)
@@ -22,6 +22,7 @@ export default async (req: Request, res: Response) => {
       errorMessage,
       latestRecallId: res.locals.latestRecallId,
       recallableCourtCases,
+      offenceNameMap: res.locals.offenceNameMap
     })
   }
 
