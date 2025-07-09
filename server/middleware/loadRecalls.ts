41254e650b7ed99c732e33793a2b3c3f7cbfcf69
Jade Alvares <jade.alvares@justice.gov.uk>
2025-07-09 16:48:26 +0100

    RCL-333 more design changes to fit new recall card design

--- server/middleware/loadRecalls.ts
+++ server/middleware/loadRecalls.ts
@@ -20,6 +20,8 @@ export default function loadRecalls(recallService: RecallService, prisonService:
     try {
       const recalls = await recallService.getAllRecalls(nomisId, user.username)
 
+      console.log('-----------recalls', JSON.stringify(recalls), undefined, 2)
+
       if (recalls && recalls.length > 0) {
         // Get location names for all recalls
         const locationIds = recalls.map(r => r.location)
