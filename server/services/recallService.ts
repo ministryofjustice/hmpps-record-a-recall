0e9723b1d172eda16d9de481fe9bf15fc7878f8e
Jade Alvares <jade.alvares@justice.gov.uk>
2025-07-29 15:46:42 +0100

    RCLL-460 update parameter in test

--- server/services/recallService.ts
+++ server/services/recallService.ts
@@ -32,6 +32,8 @@ export default class RecallService {
   async getAllRecalls(nomsId: string, username: string): Promise<Recall[]> {
     const allApiRecalls = await (await this.getApiClient(username)).getAllRecalls(nomsId)
 
+    logger.info(`Fetched recalls for NOMS ID ${nomsId}:`, allApiRecalls)
+
     return allApiRecalls.map((apiRecall: ApiRecall): Recall => this.fromApiRecall(apiRecall))
   }
 
