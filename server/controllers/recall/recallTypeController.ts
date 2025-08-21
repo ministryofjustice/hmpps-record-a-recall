709fadb7cea97d35d1e9f94dd1b5caad2fe0a5c1
james-baker-moj <159048573+james-baker-moj@users.noreply.github.com>
2025-08-21 12:53:55 +0100

    RCLL-478: Feature toggle unexpected recall type intercept (#298)
    This page is only triggered if we think the recall type selected is invalid but the rules have not been fleshed out fully yet so we are disabling it for now.
    The check is now skipped if the feature toggle is off, which it is in all environments currently.

--- server/controllers/recall/recallTypeController.ts
+++ server/controllers/recall/recallTypeController.ts
@@ -3,7 +3,8 @@ import { NextFunction, Response } from 'express'
 
 import RecallBaseController from './recallBaseController'
 import { RecallTypes } from '../../@types/recallTypes'
-import { getRecallTypeCode, sessionModelFields, getInvalidRecallTypes } from '../../helpers/formWizardHelper'
+import { getInvalidRecallTypes, getRecallTypeCode, sessionModelFields } from '../../helpers/formWizardHelper'
+import config from '../../config'
 
 export default class RecallTypeController extends RecallBaseController {
   configure(req: FormWizard.Request, res: Response, next: NextFunction) {
@@ -17,14 +18,14 @@ export default class RecallTypeController extends RecallBaseController {
   }
 
   successHandler(req: FormWizard.Request, res: Response, next: NextFunction) {
-    const selectedType = getRecallTypeCode(req)
-    const invalidRecallTypes = getInvalidRecallTypes(req)
-
-    req.sessionModel.set(
-      sessionModelFields.RECALL_TYPE_MISMATCH,
+    let recallTypeMismatch = false
+    if (config.featureToggles.unexpectedRecallTypeCheckEnabled) {
+      const selectedType = getRecallTypeCode(req)
+      const invalidRecallTypes = getInvalidRecallTypes(req)
       // @ts-expect-error Type will be correct
-      invalidRecallTypes?.map(t => t.code).includes(selectedType) || false,
-    )
+      recallTypeMismatch = invalidRecallTypes?.map(t => t.code).includes(selectedType) || false
+    }
+    req.sessionModel.set(sessionModelFields.RECALL_TYPE_MISMATCH, recallTypeMismatch)
     return super.successHandler(req, res, next)
   }
 }
