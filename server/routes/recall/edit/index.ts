6eb2a017cb42ed7ffc50ed71095f0bee1dedc366
ayo.adedeji <ayo.adedeji@justice.gov.uk>
2025-08-28 10:08:43 +0100

    Adds feature flag for migrated edit routes

--- server/routes/recall/edit/index.ts
+++ server/routes/recall/edit/index.ts
@@ -4,11 +4,21 @@ import wizard from 'hmpo-form-wizard'
 import steps from './steps'
 import populateRecallId from '../../../middleware/populateRecallId'
 import editFields from './fields'
+import populateStoredRecallRouter from './migrated/populate-stored-recall'
 
 const editRecallRouter = express.Router({ mergeParams: true })
 
+// Feature flag for Phase 7 - Edit Form Migration
+const USE_MIGRATED_EDIT_ROUTES = process.env.USE_MIGRATED_EDIT_ROUTES === 'true'
+
+editRecallRouter.use(populateRecallId())
+
+if (USE_MIGRATED_EDIT_ROUTES) {
+  // Mount migrated edit routes first - they take precedence
+  editRecallRouter.use(populateStoredRecallRouter)
+}
+
 editRecallRouter.use(
-  populateRecallId(),
   wizard(steps, editFields, {
     name: 'edit-recall',
     templatePath: 'pages/recall',
---
+++ server/routes/recall/edit/migrated/populate-stored-recall.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express'
+import FormWizard from 'hmpo-form-wizard'
+import PopulateStoredRecallController from '../../../../controllers/recall/edit/populateStoredRecallController'
+import getCourtCaseOptionsFromRas from '../../../../utils/rasCourtCasesUtils'
+import { sessionModelFields } from '../../../../helpers/formWizardHelper'
+
+const router = Router()
+
+router.get('/populate-stored-recall', async (req, res, next) => {
+  try {
+    const reqWithWizard = req as unknown as FormWizard.Request
+    const controller = new PopulateStoredRecallController({ route: '/populate-stored-recall' })
+
+    // First, set up court case items as the controller's middleware does
+    const courtCaseOptions = await getCourtCaseOptionsFromRas(reqWithWizard, res)
+    reqWithWizard.sessionModel.set(sessionModelFields.COURT_CASE_OPTIONS, courtCaseOptions)
+
+    // Configure the controller (loads recall data and adjustments)
+    await controller.configure(reqWithWizard, res, next)
+
+    // Call the locals method to populate session data
+    // This sets up all the session fields from the stored recall
+    const locals = controller.locals(reqWithWizard, res)
+
+    // Store the locals in res.locals for compatibility with templates
+    Object.assign(res.locals, locals)
+
+    // Redirect to edit summary page
+    res.redirect('/recall/edit/edit-summary')
+  } catch (error) {
+    // If there's an error, pass it to the error handler
+    next(error)
+  }
+})
+
+export default router
