eafeb2dc7f0eaeaee25e9a9c51ae07bc7a8d7524
ayoMoj <ayo.adedeji@justice.gov.uk>
2025-09-02 17:00:12 +0100

    Integrates frontend components service (#316)
    * Integrates frontend components service
    
    Enables the retrieval and rendering of header and footer components from a dedicated frontend components service.
    
    * removed footer loading

---
+++ server/middleware/getFrontendComponents.ts
@@ -0,0 +1,26 @@
+import { RequestHandler } from 'express'
+import logger from '../../logger'
+import { Services } from '../services'
+import config from '../config'
+
+export default function getFrontendComponents({ feComponentsService }: Services): RequestHandler {
+  return async (req, res, next) => {
+    if (!config.apis.frontendComponents.enabled) {
+      return next()
+    }
+
+    try {
+      const { header } = await feComponentsService.getComponents(['header'], res.locals.user.token)
+
+      res.locals.feComponents = {
+        header: header?.html,
+        cssIncludes: header?.css || [],
+        jsIncludes: header?.javascript || [],
+      }
+      return next()
+    } catch (error) {
+      logger.error(error, 'Failed to retrieve front end components')
+      return next()
+    }
+  }
+}
