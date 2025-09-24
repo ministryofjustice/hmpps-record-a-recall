f65b2cd2283fe6c5f57ca7489635a982592c0db9
Jade Alvares <jade.alvares@justice.gov.uk>
2025-09-24 14:38:01 +0100

    RCLL-476-recalls display in chronological order on home page

--- server/controllers/person/viewPersonHomeController.ts
+++ server/controllers/person/viewPersonHomeController.ts
@@ -11,10 +11,21 @@ export default async (req: Request, res: Response) => {
     const urls = getServiceUrls(nomisId)
     const useV2RecallFlow = process.env.USE_V2_RECALL_FLOW === 'true'
 
+    const sortedRecalls = [...recalls].sort((a, b) => {
+      const getDate = (recall: any) => {
+        if (recall.source === 'DPS') {
+          return recall.revocationDate ? new Date(recall.revocationDate) : new Date(0)
+        }
+        return recall.createdAt ? new Date(recall.createdAt) : new Date(0)
+      }
+
+      return getDate(b).getTime() - getDate(a).getTime() 
+    })
+
     return res.render('pages/person/home', {
       nomisId,
       prisoner,
-      recalls,
+      recalls: sortedRecalls,
       banner,
       urls,
       serviceDefinitions,
