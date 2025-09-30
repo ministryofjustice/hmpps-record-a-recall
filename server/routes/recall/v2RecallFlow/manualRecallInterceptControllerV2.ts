f6afd902d85a3c48a44879d01e97292e54953525
ayo.adedeji <ayo.adedeji@justice.gov.uk>
2025-09-30 09:36:02 +0100

    Adds manual recall intercept page for V2

---
+++ server/routes/recall/v2RecallFlow/manualRecallInterceptControllerV2.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express'
+import BaseController from '../../../controllers/base/BaseController'
+import { clearValidation } from '../../../middleware/validationMiddleware'
+import logger from '../../../../logger'
+
+export default class ManualRecallInterceptControllerV2 extends BaseController {
+  static async get(req: Request, res: Response): Promise<void> {
+    const sessionData = ManualRecallInterceptControllerV2.getSessionData(req)
+    const { nomisId, recallId } = res.locals
+
+    // Get prisoner data from session or res.locals
+    const prisoner = res.locals.prisoner || sessionData?.prisoner
+
+    // Determine if this is an edit recall flow
+    const isEditRecall = !!recallId
+
+    // Build navigation URLs
+    const backLink = `/person/${nomisId}/record-recall-v2/rtc-date`
+    const cancelUrl = `/person/${nomisId}/record-recall-v2/confirm-cancel`
+
+    // If not coming from a validation redirect, clear form responses
+    // This is an intercept page that doesn't have form fields to preserve
+    if (!res.locals.formResponses) {
+      res.locals.formResponses = {}
+    }
+
+    res.render('pages/recall/v2/manual-recall-intercept', {
+      prisoner,
+      nomisId,
+      isEditRecall,
+      backLink,
+      cancelUrl,
+      validationErrors: res.locals.validationErrors,
+      formResponses: res.locals.formResponses,
+    })
+  }
+
+  static async post(req: Request, res: Response): Promise<void> {
+    const { nomisId } = res.locals
+
+    // Mark that the user has confirmed they understand the manual process
+    ManualRecallInterceptControllerV2.updateSessionData(req, {
+      'select-court-case-details.njk': 'confirmed',
+    })
+
+    logger.info(`User confirmed manual recall intercept for nomisId: ${nomisId}`)
+
+    // Clear validation and redirect to next step
+    clearValidation(req)
+    return res.redirect(`/person/${nomisId}/record-recall-v2/select-cases`)
+  }
+}
