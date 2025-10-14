606e3d8bb51f82c406ec525af229fec1f12528b6
Jade Alvares <jade.alvares@justice.gov.uk>
2025-10-14 11:35:43 +0100

    RCLL-531 replaced Draft to match new types

--- server/controllers/recall/v2RecallFlow/selectCourtCaseControllerV2.ts
+++ server/controllers/recall/v2RecallFlow/selectCourtCaseControllerV2.ts
@@ -219,7 +219,7 @@ export default class SelectCourtCaseControllerV2 extends BaseController {
           const enhancedCases = res.locals.recallableCourtCases as EnhancedRecallableCourtCase[]
 
           reviewableCases = enhancedCases
-            .filter(c => c.status !== 'DRAFT' && c.isSentenced)
+            .filter(c => c.status !== 'ACTIVE' && c.isSentenced)
             .map(recallableCase => {
               const caseReference = recallableCase.reference?.trim() || ''
 
