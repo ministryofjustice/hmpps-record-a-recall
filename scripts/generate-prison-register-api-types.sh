4df5a52a8f3ab0ddb2472308ed403630e416dceb
James Baker <james.baker3@digital.justice.gov.uk>
2025-10-29 14:17:08 +0000

    RCLL-555: remove scripts that were already in package.json

--- scripts/generate-prison-register-api-types.sh
+++
@@ -1,2 +0,0 @@
-npx openapi-typescript https://prison-register-dev.hmpps.service.justice.gov.uk/v3/api-docs | sed "s/\"/'/g" | sed "s/;//g" > ../server/@types/prisonRegisterApi/index.d.ts
-eslint --fix "../server/@types/prisonRegisterApi/index.d.ts"
--- scripts/generate-ras-api-types.sh
+++
@@ -1,2 +0,0 @@
-npx openapi-typescript https://remand-and-sentencing-api-dev.hmpps.service.justice.gov.uk/v3/api-docs | sed "s/\"/'/g" | sed "s/;//g" > ../server/@types/remandAndSentencingApi/index.d.ts
-eslint --fix "../server/@types/remandAndSentencingApi/index.d.ts"
