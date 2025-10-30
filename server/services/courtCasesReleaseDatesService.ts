ed9cb1ba21acf3832a7a4f7e5c00e8632d86ba87
Yasin Mustafa <yasin.mustafa@gmail.com>
2025-10-30 16:20:34 +0000

    lint fix

--- server/services/courtCasesReleaseDatesService.ts
+++ server/services/courtCasesReleaseDatesService.ts
@@ -1,7 +1,7 @@
 import { CcrdServiceDefinitions } from '../@types/courtCasesReleaseDatesApi/types'
 import CourtCasesReleaseDatesApiClient from '../data/courtCasesReleaseDatesApiClient'
 
-    export default class CourtCasesReleaseDatesService {
+export default class CourtCasesReleaseDatesService {
   constructor(private readonly courtCasesReleaseDatesApiClient: CourtCasesReleaseDatesApiClient) {}
 
   public async getServiceDefinitions(prisonerId: string, token: string): Promise<CcrdServiceDefinitions> {
