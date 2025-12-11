5ddfa46ded05201e7647207cb3321858de19399a
James Baker <james.baker3@digital.justice.gov.uk>
2025-12-11 09:21:53 +0000

    RCLL-609: Allow npm install
    Initial setup using npx @ministryofjustice/hmpps-npm-script-allowlist and only allowing cypress to run a script.
    See https://github.com/ministryofjustice/hmpps-typescript-lib/tree/main/packages/npm-script-allowlist

---
+++ .allowed-scripts.mjs
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
+
+export default configureAllowedScripts({
+  allowlist: {
+    'node_modules/@parcel/watcher@2.5.1': 'FORBID',
+    'node_modules/cypress@15.7.1': 'ALLOW',
+    'node_modules/dtrace-provider@0.8.8': 'FORBID',
+    'node_modules/esbuild@0.25.12': 'FORBID',
+    'node_modules/fsevents@2.3.3': 'FORBID',
+    'node_modules/unrs-resolver@1.11.1': 'FORBID',
+  },
+})
