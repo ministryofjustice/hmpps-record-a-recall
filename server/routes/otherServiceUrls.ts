dfcc8e8054496442022dbc637c00f5aa88e395b9
Yasin Mustafa <yasin.mustafa@gmail.com>
2025-12-12 19:19:19 +0000

    code tidy

--- server/routes/otherServiceUrls.ts
+++ server/routes/otherServiceUrls.ts
@@ -4,5 +4,5 @@ export default class OtherServiceUrls {
   static rasDashboard = (nomsId: string) => `${config.urls.remandAndSentencing}/person/${nomsId}`
 
   static rasUnknownSentenceTypes = (nomsId: string, params: string) =>
-    `${config.urls.remandAndSentencing}/person/${nomsId}/unknown-recall-sentence?${params.toString()}`
+    `${config.urls.remandAndSentencing}/person/${nomsId}/unknown-recall-sentence?${params}`
 }
