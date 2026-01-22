170c2eac9b060710ea3a03bc7b0b2a5abb063569
Yasin Mustafa <yasin.mustafa@gmail.com>
2026-01-22 17:54:28 +0000

    fix int tests

---
+++ integration_tests/mockApis/frontEndComponentsApi.ts
@@ -0,0 +1,28 @@
+import type { SuperAgentRequest } from 'superagent'
+import { stubFor } from './wiremock'
+
+export default {
+  stubPing: (httpStatus = 200): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/frontend-components/ping',
+      },
+      response: {
+        status: httpStatus,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: { status: httpStatus === 200 ? 'UP' : 'DOWN' },
+      },
+    }),
+  stubComponents: (httpStatus = 200): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/frontend-components/component?component=header s',
+      },
+      response: {
+        status: httpStatus,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+      },
+    }),
+}
