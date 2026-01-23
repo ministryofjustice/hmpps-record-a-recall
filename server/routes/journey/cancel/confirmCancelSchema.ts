de775a18ad6cba09960ac3c35203c2e87e809fc7
Yasin Mustafa <yasin.mustafa@gmail.com>
2026-01-23 10:09:06 +0000

    RCLL-670 upgrade zod (and fix backwards incompatibility issue)

--- server/routes/journey/cancel/confirmCancelSchema.ts
+++ server/routes/journey/cancel/confirmCancelSchema.ts
@@ -3,10 +3,10 @@ import { z } from 'zod'
 const REQUIRED_ERROR = 'Select whether you want to cancel or not'
 
 export const confirmCancelSchema = z.object({
-  confirmCancel: z
-    .string()
-    .optional()
-    .refine(val => val === 'YES' || val === 'NO', { message: REQUIRED_ERROR }),
+  confirmCancel: z.preprocess(
+    v => (v === '' ? undefined : v),
+    z.unknown().refine(v => v === 'YES' || v === 'NO', { message: REQUIRED_ERROR }),
+  ),
 })
 
 export type ConfirmCancelForm = z.infer<typeof confirmCancelSchema>
--- server/routes/journey/manual/select-cases/selectCourtCasesSchema.ts
+++ server/routes/journey/manual/select-cases/selectCourtCasesSchema.ts
@@ -3,10 +3,10 @@ import { z } from 'zod'
 const REQUIRED_ERROR = 'Select whether this case had an active sentence'
 
 export const selectCourtCasesSchema = z.object({
-  activeSentenceChoice: z
-    .string()
-    .optional()
-    .refine(val => val === 'YES' || val === 'NO', { message: REQUIRED_ERROR }),
+  activeSentenceChoice: z.preprocess(
+    v => (v === '' ? undefined : v),
+    z.unknown().refine(v => v === 'YES' || v === 'NO', { message: REQUIRED_ERROR }),
+  ),
 })
 
 export type SelectCourtCasesForm = z.infer<typeof selectCourtCasesSchema>
