3b3d78d1138cffb97f8ae9a19efd78da6b843660
Jade Alvares <jade.alvares@justice.gov.uk>
2026-03-31 11:18:18 +0100

    allow-no-active-bookings-offenders-to-make-recall

--- server/middleware/populateCurrentPrisoner.ts
+++ server/middleware/populateCurrentPrisoner.ts
@@ -3,6 +3,7 @@ import logger from '../../logger'
 import PrisonerSearchService from '../services/prisonerSearchService'
 import { PrisonUser } from '../interfaces/hmppsUser'
 import FullPageError from '../model/FullPageError'
+import config from '../config'
 
 export default function populateCurrentPrisoner(prisonerSearchService: PrisonerSearchService): RequestHandler {
   return async (req, res, next) => {
@@ -13,11 +14,15 @@ export default function populateCurrentPrisoner(prisonerSearchService: PrisonerS
       try {
         const prisoner = await prisonerSearchService.getPrisonerDetails(nomsId, username)
         res.locals.prisoner = prisoner
-        if (
-          prisoner.prisonId === 'OUT' ||
-          !caseLoads.map(caseload => caseload.caseLoadId).includes(prisoner.prisonId)
-        ) {
-          throw FullPageError.notInCaseLoadError()
+        if (!config.featureToggles.allowInactiveBookings) {
+          console.log('**************AAAAA')
+          console.log('FLAG VALUE:', config.featureToggles.allowInactiveBookings)
+          if (
+            prisoner.prisonId === 'OUT' ||
+            !caseLoads.map(caseload => caseload.caseLoadId).includes(prisoner.prisonId)
+          ) {
+            throw FullPageError.notInCaseLoadError()
+          }
         }
       } catch (error) {
         logger.error(error, `Failed to get prisoner with prisoner number: ${nomsId}`)
